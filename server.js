190ae0372495eda526d2bc688d18d94e558b90ad
Krishna Teja <krishna.cse.2006@gmail.com>
2018-04-10 00:05:47 +0530

    first checkin

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+app.get('/api/hello', (req, res) => {
+  res.send( 'BHA RA TA  Under Construction... Coming Soon...' );
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ src/js/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+      return <h1>The site is under construction...coming soon}</h1>;
+    }
+  }
